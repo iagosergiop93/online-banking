920f490171fb1812cfb438597d668f0ed35fd781
iagosergiop93 <iagosergiop93@gmail.com>
2020-08-09 20:28:29 -0400

    env.prod file

--- web/online-bank/src/environments/environment.prod.ts
+++ web/online-bank/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+
+export const API_URL = "http://localhost:4201";
+export const TOKEN = {
+  name: "b-token",
+  scheme: "Bearer",
+  userInfo: "user-info"
+};
