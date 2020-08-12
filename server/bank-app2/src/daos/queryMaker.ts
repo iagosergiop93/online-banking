34ab36a7cbfb5b759437328cd17e21e187924462
iagosergiop93 <iagosergiop93@gmail.com>
2020-08-12 10:47:31 -0400

    Fix feedback error

--- server/bank-app2/src/daos/queryMaker.ts
+++ server/bank-app2/src/daos/queryMaker.ts
@@ -24,7 +24,7 @@ export function startTransaction(): Promise<PoolConnection> {
         getPoolConnection()
         .then((conn) => {
             conn.beginTransaction(err => {
-                if(err) throw new ServerError("Error beginning transaction.");
+                if(err) reject(new ServerError("Error beginning transaction."));
                 resolve(conn);
             });
         })
